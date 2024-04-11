12b1c3846999f76c75950f2c14d19ae8851110af
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-11 12:41:42 +0300

    make the process asynchronous

---
+++ src/main/java/com/captian/reactor/section_one/Lec06SupplierRefactoring.java
@@ -0,0 +1,27 @@
+package com.captian.reactor.section_one;
+
+import com.captian.reactor.util.Util;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+public class Lec06SupplierRefactoring {
+
+    public static void main(String[] args) {
+        getName();
+        getName()
+                .subscribeOn(Schedulers.boundedElastic())
+                .subscribe(Util.onNext()
+        );
+        getName();
+        Util.sleepSeconds(4);
+    }
+
+    public static Mono<String> getName() {
+        System.out.println("Entered getName method");
+        return Mono.fromSupplier(() -> {
+            System.out.println("Generating name...");
+            Util.sleepSeconds(3);
+            return Util.faker().name().fullName();
+        }).map(String::toUpperCase);
+    }
+}
