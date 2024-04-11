3b0f3d37ca96d9fe11c3fd2d233bac369f979e29
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-11 12:56:59 +0300

    block the process as you wait for response

--- src/main/java/com/captian/reactor/section_one/Lec06SupplierRefactoring.java
+++ src/main/java/com/captian/reactor/section_one/Lec06SupplierRefactoring.java
@@ -7,11 +7,14 @@ import reactor.core.scheduler.Schedulers;
 public class Lec06SupplierRefactoring {
 
     public static void main(String[] args) {
+
         getName();
-        getName()
+
+        String name = getName()
                 .subscribeOn(Schedulers.boundedElastic())
-                .subscribe(Util.onNext()
-        );
+                .block();
+        System.out.println(name);
+
         getName();
         Util.sleepSeconds(4);
     }
