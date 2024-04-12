5f498838c05efaf3bfaf6b2ad07e247649fb9a6b
Jackson G. Mwaniki <jackson.gitundu@gmail.com>
2024-04-12 11:32:00 +0300

    add logs to helo in debugging

--- src/main/java/com/captian/reactor/section_two/Lec05FluxRange.java
+++ src/main/java/com/captian/reactor/section_two/Lec05FluxRange.java
@@ -8,7 +8,9 @@ public class Lec05FluxRange {
     public static void main(String[] args) {
 
         Flux.range(3, 10)
+                .log()
                 .map(i -> Util.faker().name().fullName())
+                .log()
                 .subscribe(
                         Util.onNext()
                 );
